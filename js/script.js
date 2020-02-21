85489f9e212ffca0e6aea4c3cc221f46bcc0c8c5
resascreative <tsmithgd@gmail.com>
2020-02-20 21:17:09 -0800

    fortune

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-function halfNumber() {
-    let 
+// function halfNumber() {
+//     let 
+// }
+
+//Fortune Teller
+function tellFortune(jobTitle, geoLocation, partner, numKids) {
+    var future = 'You will be a ' + job + " in ' + location + "and married to" +
+   spouse + ' ' + ' with ' + Kids + ' kids.';
+    console.log(future);
 }
+
+tellFortune('bball player', 'spain', 'Shaq', 3);
+tellFortune('stunt double', 'Japan', 'Ryan Gosling', 3000);
+tellFortune('Elvis impersonator', 'Russia', 'The Oatmeal', 0);
